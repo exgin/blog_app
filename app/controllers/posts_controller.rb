59b46de7ef9c65cd91a6a549b76d6284250f680a
sammy <exgin3538@yahoo.com>
2020-10-15 15:57:38 -0400

    finish all posts basic routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,6 +40,13 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
+    end
+
     # a private class method
     private
     def post_params
